cc9153b6e7f1da518cd5b6be5502002922f6d3d0
Kidus Kibrom <kidusc941@gmail.com>
2026-01-03 17:05:02 +0300

    fixed the basic API endpoint working at /api/v1/courses/

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,27 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import CourseSerializer
-from .models import Course
-from django.http import HttpResponse
 
 # Create your views here.
-"""
-for testing
 
-"""
-def index(request):
-    return HttpResponse("Hello, world")
-
-#regualr django view not API for HTML page 
-def course_list(request):
-    courses = Course.objects.all()
-    return render(request, 'courses/list.html', {'courses': courses})
-
-#for API endpoint
 @api_view(['GET'])
 def course_list_api(request):
-    courses = Course.objects.all()
-    serializer = CourseSerializer(courses, many=True)
-    return Response(serializer.data)
+    return Response({
+        'message': 'API is working!',
+        'endpoint': '/api/v1/courses/',
+        'next_steps': 'We will add real course data next'
+    })
+
 
