8515328567d6fa79b6ad4c382bedc0e0a660abe5
Kidus Kibrom <kidusc941@gmail.com>
2026-01-04 12:29:00 +0300

    learning and added CORS for fronted communication, tested API endpoint, understanding REST APIS

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Course
 from .serializers import CourseSerializer
-
+#this API end point get data from Database, convert to JSON format, send to fronted
 @api_view(['GET'])
 def course_list_api(request):
     courses = Course.objects.all()
